712d74530719df8c8aad3612f7fe448c1e6a1c1c
IsaqueA <84244850+seraphritt@users.noreply.github.com>
2024-08-29 16:12:33 -0300

    edit: solution1

--- main.py
+++ main.py
@@ -144,4 +144,4 @@ dataset = Dataset.from_dict(data)
 #plt.savefig("evaluation_results.pdf")
 #plt.close()
 while True:
-    get_response(input() + "Remember to always answer in Portuguese" , chain)
+    get_response(input(), chain)
