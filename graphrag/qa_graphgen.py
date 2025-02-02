abe3e3abe5ca2781789a3545c023e512ad5ebbad
seraphritt <isak369@hotmail.com>
2025-02-02 20:12:56 -0300

    adicionando

--- graphrag/qa_graphgen.py
+++ graphrag/qa_graphgen.py
@@ -42,7 +42,7 @@ dicio = {}
 ground_truth = qa.get_second()
 perguntas = qa.get_first()
 count = 0
-for pergunta in perguntas[:100]:
+for pergunta in perguntas[:95]:
     try:
         print(count)
         resposta = rag.query(f"Responda em Português: {pergunta}", param=QueryParam(mode="local"))
