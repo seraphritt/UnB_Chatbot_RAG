cb456eac6ba5094228ba374d2574c2264aa21f9d
seraphritt <isak369@hotmail.com>
2025-02-02 22:28:31 -0300

    testando llama3.2

--- graphrag/eval.py
+++ graphrag/eval.py
@@ -42,7 +42,7 @@ data_samples = {
     "ground_truth": ground_truths,
 }
 
-models = ["llama3.1:latest"]
+models = ["llama3.2:3b:latest"]
 for model in models:
     # file_name = f"qa_{model}.json"
     # with open(file_name, "r", encoding="utf-8") as json_file:
