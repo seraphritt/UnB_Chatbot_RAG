54a1fa9ec11daebf2b3328058c05f033322f110d
seraphritt <isak369@hotmail.com>
2025-02-07 10:01:05 -0300

    adicionando eval do gemma

--- eval.py
+++ eval.py
@@ -79,8 +79,7 @@ for model in models:
     print(df)
     csv_file_name = f"evaluation_results_saude{model_name}.csv"
     df.to_csv(csv_file_name, index=False, encoding='utf-8')
-    categories = ['context_precision', 'answer_relevancy', 'context_recall', 'faithfulness', 'semantic_similarity', 'context_entity_recall', 'answer_correctness']
-    print(df.keys())
+    categories = list(df.keys())
     data = [df[category].dropna() for category in categories]  # Drop NaN 
     plt.figure(figsize=(15, 6))
     plt.boxplot(data, vert=True, patch_artist=True, tick_labels=['Context Precision', 'Answer Relevancy', 'Context Recall', 
--- graphrag/eval.py
+++ graphrag/eval.py
@@ -35,25 +35,25 @@ with open(file_name, "r", encoding="utf-8") as json_file:
 questions = [
     (data[str(x)][0]["question"] if isinstance(data[str(x)], list) else "")
     if str(x) in data else ""
-    for x in range(311)
+    for x in range(100)
 ]
 
 answers = [
     (data[str(x)][0]["answer"] if isinstance(data[str(x)], list) else "")
     if str(x) in data else ""
-    for x in range(311)
+    for x in range(100)
 ]
 
 ground_truths = [
     (data[str(x)][0]["ground_truth"] if isinstance(data[str(x)], list) else "")
     if str(x) in data else ""
-    for x in range(311)
+    for x in range(100)
 ]
 
 contexts = [
     ([data[str(x)][0]["context"]] if isinstance(data[str(x)], list) else [""])
     if str(x) in data else [""]
-    for x in range(311)
+    for x in range(100)
 ]
 
 data_samples = {
@@ -96,10 +96,10 @@ for model in models:
 
     print(result)
     df = result.to_pandas()
-    csv_file_name = f"evaluation_results_GRAPH_{model_name}.csv"
+    csv_file_name = f"evaluation_results_GRAPH_UNB{model_name}.csv"
     df.to_csv(csv_file_name, index=False, encoding='utf-8')
     print(df.keys())
-    categories = ['context_precision', 'answer_relevancy', 'context_recall', 'faithfulness', 'semantic_similarity', 'context_entity_recall', 'answer_correctness']
+    categories = list(df.keys())
     data = [df[category].dropna() for category in categories]  # Drop NaN 
     plt.figure(figsize=(15, 6))
     plt.boxplot(data, vert=True, patch_artist=True, tick_labels=['Context Precision', 'Answer Relevancy', 'Context Recall', 
@@ -108,8 +108,8 @@ for model in models:
     plt.ylabel('Valores')
     plt.title(f'Distribução das Métricas (RAGAS) no modelo {model_name}')
 
-    with PdfPages(f'boxplot_graph_{model_name}.pdf') as pdf:
+    with PdfPages(f'boxplot_graph_UNB{model_name}.pdf') as pdf:
         pdf.savefig()
         plt.close()
-    print(f"Documento evaluation_results_{model_name}.csv e boxplot (boxplot_graph_{model_name}_teste.pdf) criados")
+    print(f"Documento evaluation_results_UNB{model_name}.csv e boxplot (boxplot_graph_UNB{model_name}_teste.pdf) criados")
 
